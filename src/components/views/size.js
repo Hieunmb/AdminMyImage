40b9c76b4472523bcc0fc26aed365cf781be7923
Nguyễn Hiếu <zhuongdae@gmail.com>
2024-01-05 23:45:41 +0700

    delete frame size

--- src/components/views/size.js
+++ src/components/views/size.js
@@ -1,8 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import api from "../../services/api";
+import url from "../../services/url";
 
 function Size(props){
     const product = props.product;
-    console.log(props.product);
+    const navigate= useNavigate();
+    const handleDelete = async () => {
+        try {
+            await api.delete(`${url.SIZE.DELETE}/?id=${product.id}`);
+            window.alert(`Product with size name ${product.sizeName} has been deleted.`);
+            navigate("/sizes");
+            // Optionally, you can trigger a refresh or update the UI after deletion
+            // For instance, you might want to remove the deleted item from the list
+        } catch (error) {
+            console.error("Error deleting product:", error);
+            // Handle errors, show an alert, or perform specific actions
+        }
+    };
     return(
         <tr>
             <td>{product.id}</td>
@@ -10,7 +24,8 @@ function Size(props){
              <td>{product.sizeWidth}</td>
              <td>{product.sizeHeight}</td>
              <td>${product.sizeAmount}.00</td>
-            <td><Link to={`/sizes_edit/${product.id}`} class="text-inverse p-r-10" data-bs-toggle="tooltip" title="" data-original-title="Edit"><i class="ti-marker-alt"></i></Link> <a href="#" class="text-inverse" title="" data-bs-toggle="tooltip" data-original-title="Delete"><i class="ti-trash"></i></a></td>
+            <td><Link to={`/sizes_edit/${product.id}`} class="text-inverse p-r-10" data-bs-toggle="tooltip" title="" data-original-title="Edit"><i class="ti-marker-alt"></i></Link>
+            <a onClick={handleDelete} class="text-inverse" title="" data-bs-toggle="tooltip" data-original-title="Delete"><i class="ti-trash"></i></a></td>
         </tr>
     )
 }
